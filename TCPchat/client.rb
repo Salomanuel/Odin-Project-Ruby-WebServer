3cb9c3a20c99c84ec9dc4f5ea4d050200ce73042
Manuel <salomanuel.aux+code@gmail.com>
2016-09-17 17:53:50 +0200

    chat works!

--- TCPchat/client.rb
+++ TCPchat/client.rb
@@ -5,5 +5,31 @@ class Client
 		@server   = server
 		@request  = nil
 		@response = nil
+		listen
+		send
+		@request.join
+		@response.join
+	end
+
+	def send
+		puts "Enter the username:"
+		@request = Thread.new do
+			loop {
+				msg = $stdin.gets.chomp
+				@server.puts ( msg )
+			}
+		end
+	end
+
+	def listen
+		@response = Thread.new do
+			loop {
+				msg = @server.gets.chomp
+				puts "#{msg}"
+			}
+		end
 	end
 end
+
+server = TCPSocket.open( "localhost", 3000 )
+Client.new( server )
