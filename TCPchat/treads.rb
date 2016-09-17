003950f5fb282705d5776330bb1abdfa86688640
Manuel <salomanuel.aux+code@gmail.com>
2016-09-17 15:50:10 +0200

    thread \example 1

--- TCPchat/treads.rb
+++ TCPchat/treads.rb
@@ -1,4 +1,5 @@
 def calculate_sum(arr)
+	sleep(2)
 	sum = 0
 	arr.each do |item|
 		sum += item
@@ -10,6 +11,15 @@ end
 @items2 = [45, 90,  2]
 @items3 = [99, 22, 31]
 
-puts "items1 = #{calculate_sum(@items1)}"
-puts "items2 = #{calculate_sum(@items2)}"
-puts "items3 = #{calculate_sum(@items3)}"
+threads = (1..3).map do |i|
+	Thread.new(i) do |i|
+		items = instance_variable_get("@items#{i}")
+		puts "items#{i} = #{calculate_sum(items)}"
+	end
+end
+
+threads.each { |t| t.join }
+
+#puts "items1 = #{calculate_sum(@items1)}"
+#puts "items2 = #{calculate_sum(@items2)}"
+#puts "items3 = #{calculate_sum(@items3)}"
