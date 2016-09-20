68f6a399eb9f37ca22caee09c608752100d990d7
Manuel <salomanuel.aux+code@gmail.com>
2016-09-20 15:30:26 +0200

    odinserver.rb and simpleodinserver.rb underway

--- socket/simpleodinserver.rb
+++ socket/simpleodinserver.rb
@@ -13,12 +13,13 @@ loop do
 	request_line2 = "#{request} was requested"
 
 	if request[0] == "GET" and request[1] == "/index.html"
+		page = File.read("index.html")
 		socket.print "HTTP/1.1 200 OK\r\n" 											 		+
-								 "Content-Type: 	text/plain\r\n" 				 			+
-								 "Content-Lenght: #{request_line2.bytesize}\r\n"+
+								 "Content-Type: 	text/html\r\n" 				 			+
+								 "Content-Lenght: #{page.bytesize}\r\n"+
 								 "Connection: 		close\r\n"
 		socket.print "\r\n"
-		socket.print "will show index.html"
+		socket.print page
 	else								
 		socket.print "HTTP/1.1 200 OK\r\n" 											 		+
 								 "Content-Type: 	text/plain\r\n" 				 			+
