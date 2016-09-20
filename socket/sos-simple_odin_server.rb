e6fc3efae878a884d402fba0a5239be8470ecdd9
Manuel <salomanuel.aux+code@gmail.com>
2016-09-20 15:49:11 +0200

    bettered SOS with a new dynamic search for the file to gather (file.exist?)

--- socket/sos-simple_odin_server.rb
+++ socket/sos-simple_odin_server.rb
@@ -11,9 +11,10 @@ loop do
 
 	request_line  = "request was #{request}"
 	request_line2 = "#{request} was requested"
+	not_found 		= "page not found, type better"
 
-	if request[0] == "GET" and request[1] == "/index.html"
-		page = File.read("index.html")
+	if request[0] == "GET" and File.exist?request[1][1..-1]
+		page = File.read(request[1][1..-1])		#filename without "/"
 		socket.print "HTTP/1.1 200 OK\r\n" 											 		+
 								 "Content-Type: 	text/html\r\n" 				 			+
 								 "Content-Lenght: #{page.bytesize}\r\n"+
@@ -21,12 +22,13 @@ loop do
 		socket.print "\r\n"
 		socket.print page
 	else								
-		socket.print "HTTP/1.1 200 OK\r\n" 											 		+
+		content = "#{not_found}\n#{request[1][1..-1]}"
+		socket.print "HTTP/1.1 404 Not Found\r\n" 											 		+
 								 "Content-Type: 	text/plain\r\n" 				 			+
-								 "Content-Lenght: #{request_line.bytesize}\r\n" +
+								 "Content-Lenght: #{content.bytesize}\r\n" +
 								 "Connection: 		close\r\n"
 		socket.print "\r\n"
-		socket.print request_line
+		socket.print content
 	end
 	socket.close
 end
