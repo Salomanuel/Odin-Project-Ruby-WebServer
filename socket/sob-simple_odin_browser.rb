a87ce1ef2dcc95f1d1cdebe8b90eae80da9838cf
Manuel <salomanuel.aux+code@gmail.com>
2016-09-20 17:07:26 +0200

    interface for SOB

--- socket/sob-simple_odin_browser.rb
+++ socket/sob-simple_odin_browser.rb
@@ -1,12 +1,38 @@
 require 'socket'
 
-host = 'localhost'
+@host = 'localhost'
 port = 2345
-path = '/index2.html'
+@path = '/'
 
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+def pages_options
+	puts "what's page you want to reach in #{@host}'s domain?"
+	puts "1 for index.html, 2 for thanks.html, 3 for custom"
+	case gets.chomp
+	when "1" then @path << "index.html"
+	when "2" then @path << "thanks.html"
+	when "3" then puts "write the page you want"; @path << gets.chomp
+	else 
+		@path << "index.html"
+	end
+		
+	puts "1 for GET, 2 for POST (default is GET)"
+	case gets.chomp
+	when "1" then @get_or_post = "GET"
+	when "2" then @get_or_post = "POST"
+	else 				
+		@get_or_post = "GET"
+	end
+end
 
-socket  = TCPSocket.open(host,port)
+def post
+
+end
+
+pages_options
+#post if @get_or_post == "POST"
+request = "#{@get_or_post} #{@path} HTTP/1.0\r\n\r\n"
+
+socket  = TCPSocket.open(@host,port)
 socket.print(request)
 response = socket.read
 headers, body = response.split("\r\n\r\n", 2)
